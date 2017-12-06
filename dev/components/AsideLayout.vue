2ecefd1a62c35f759660d31d598af5fd4b5e0548
Mac <mac@Lijia-Wang.local>
2017-12-06 22:31:57 +0800

    add tree list

---
+++ dev/components/AsideLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout row wrap fill-height>
+    <v-flex xs12 sm3 fill-height v-show="aside">
+      <div style="height: 100%" class="white elevation-1">
+        <slot name="aside"></slot>
+      </div>
+    </v-flex>
+    <v-flex xs12 v-bind="{ sm9: aside, sm12: !aside }">
+      <v-toolbar flat>
+        <v-toolbar-side-icon @click.stop="aside = !aside"></v-toolbar-side-icon>
+        <v-toolbar-title>Product</v-toolbar-title>
+      </v-toolbar>
+      <slot></slot>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    props: {
+      aside: {
+        type: Boolean,
+        default: true
+      }
+    }
+  }
+</script>
--- dev/components/TreeList.vue
+++ dev/components/TreeList.vue
@@ -1,47 +1,49 @@
 <template>
-  <template v-for="(item, i) in items">
-    <v-list-group v-if="item.children" v-bind:key="item.text" :prepend-icon="item.action" no-action>
-      <v-list-tile slot="activator">
-        <v-list-tile-content>
-          <v-list-tile-title>{{ item.text }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-      <template v-for="(child, n) in item.children">
-        <v-list-group v-if="child.children" v-bind:key="child.text" :prepend-icon="child.action" sub-group>
-          <v-list-tile slot="activator">
-            <v-list-tile-content>
-              <v-list-tile-title>{{ child.text }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-          <template v-for="(sub, n) in child.children">
-            <v-list-group v-if="sub.children" v-bind:key="sub.text" :prepend-icon="sub.action" sub-group>
-              <v-list-tile slot="activator">
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ sub.text }}</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile v-for="c in sub.children" v-bind:key="c.text" @click="">
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ c.text }}</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-list-group>
-          </template>
-        </v-list-group>
-        <v-list-tile v-else @click="">
-          <v-list-tile-content>
-            <v-list-tile-title>{{ child.text }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </template>
-    </v-list-group>
-    <v-list-tile v-else @click="">
-      <v-list-tile-action>
-        <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
-      </v-list-tile-action>
+  <v-list-group v-if="isNode" v-bind="{ 'sub-group': !root }">
+    <v-list-tile slot="activator" ripple>
       <v-list-tile-content>
-        <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+        <v-list-tile-title>{{ model.text }}</v-list-tile-title>
       </v-list-tile-content>
     </v-list-tile>
-  </template>
+    <v-tree-list v-for="(item, index) in model.children" :key="index" :model="item"></v-tree-list>
+  </v-list-group>
+  <v-list-tile v-bind="{
+    to: !model.target ? model.href : null,
+    href: model.target && model.href
+  }" :disabled="model.disabled" :target="model.target" v-else ripple>
+    <v-list-tile-action>
+      <v-icon v-if="model.icon">{{ model.icon }}</v-icon>
+    </v-list-tile-action>
+    <v-list-tile-content>
+      <v-list-tile-title>{{ model.text }}</v-list-tile-title>
+    </v-list-tile-content>
+    <v-chip v-if="model.badge" color="red lighten-1" text-color="white" small disabled>{{ model.badge }}</v-chip>
+  </v-list-tile>
 </template>
+
+<script>
+  export default {
+    props: {
+      model: Object,
+      root: Object
+    },
+    data() {
+      return {
+        open: true
+      }
+    },
+    computed: {
+      isNode() {
+        return this.model.children &&
+          this.model.children.length
+      }
+    },
+    methods: {
+      toggle() {
+        if (this.isNode) {
+          this.open = !this.open
+        }
+      }
+    }
+  }
+</script>
